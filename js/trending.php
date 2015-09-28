54f2c3b51eacbc0cb43e69a52564d35d3da2dff8
catnpete <catnpete@gmail.com>
2015-09-28 13:10:18 +0800

    Repeat Analysis tab: Replace trending.php
    To display 4 bars for repeat analysis

--- js/trending.php
+++ js/trending.php
@@ -6,9 +6,63 @@
     
     $conn = mysqli_connect($host, $username, $password, $database);
     //$connection = mysql_select_db($database, $conn);
-
+ 
     $myquery = "
-SELECT k1.`NRIC`, k1.`VisitFY`, k2.`VisitFY`, k1.`M_Systolic_1st`, k1.`M_Diastolic_1st`, k1.`L_Chol_f`, 
+    SELECT * 
+    FROM (
+    select *
+    from `ktphalldata`
+    WHERE `revisit` = 'TRUE' 
+    ORDER BY `NRIC`,`Measurement.Att.Date` ASC 
+    ) as test
+    GROUP BY `NRIC`
+UNION
+    SELECT * 
+    FROM (
+    select *
+    from `ktphalldata`
+    WHERE `revisit` = 'TRUE' 
+    ORDER BY `NRIC`,`Measurement.Att.Date` DESC 
+    ) as test
+    GROUP BY `NRIC`
+    order by `NRIC`
+    ";    
+  /* 
+  
+  SELECT * 
+    FROM `ktphalldata`
+    WHERE `revisit` = 'TRUE'
+    ORDER BY `NRIC`, `Measurement.Att.Date` DESC
+    
+    
+  SELECT * FROM `ktphalldata`   
+WHERE `Measurement.Att.Date`> '2014-01-01' AND 
+`Measurement.Att.Date`< '2014-12-31'
+ORDER BY `Measurement.Att.Date` ASC
+  
+  
+    SELECT * 
+    FROM (
+    select *
+    from `ktphalldata`
+    WHERE `revisit` = 'TRUE'
+    ORDER BY `NRIC`,`Measurement.Att.Date` ASC 
+    ) as test
+    GROUP BY `NRIC`
+UNION
+    SELECT * 
+    FROM (
+    select *
+    from `ktphalldata`
+    WHERE `revisit` = 'TRUE'
+    ORDER BY `NRIC`,`Measurement.Att.Date` DESC 
+    ) as test
+    GROUP BY `NRIC`
+    order by `NRIC`
+    
+    
+    $myquery = "
+SELECT k1.`Healthy`, k1.`Habits`, k1.`NRIC`, k1.`VisitFY`, k2.`VisitFY`, k1.`M_Systolic_1st`, k1.`M_Diastolic_1st`, k1.`L_Chol_f`, 
 k1.`L_Trig_f`, k1.`L_HDL_f`, k1.`L_LDL_f`, k1.`L_Glucose_f`, k1.`f_BMI`, 
 k1.`revisit`, k1.`NurseAction`, k1.`Measurement.Att.Date`,
 IF(k1.`Measurement.Att.Date` < k2.`Measurement.Att.Date`,'Past','Current') AS PastOrCurrent
@@ -18,6 +72,8 @@ WHERE k1.`revisit` = 'TRUE' and (k1.`NurseAction` = 'Y' OR k1.`NurseAction` = 'T
 GROUP BY k1.`NRIC`, k1.`Measurement.Att.Date`
 ORDER BY k1.`NRIC`, k1.`VisitFY`
 ";
+*/
+
 /*
     $myquery1 = "
         SELECT * 
@@ -47,13 +103,13 @@ ORDER BY k1.`NRIC`, k1.`VisitFY`
     for ($x = 0; $x < mysqli_num_rows($query); $x++) {
         $nodes[] = mysqli_fetch_assoc($query);
     }
-
-        $links = array();
+/*
+    $links = array();
     
     for ($x = 0; $x < mysqli_num_rows($query); $x++) {
         $links[] = mysqli_fetch_assoc($query);
     }
-/*    
+   
 echo "{";
 echo '"links": ', json_encode($links), "\n";
 echo ',"nodes": ', json_encode($nodes), "\n";
